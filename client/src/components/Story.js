3d9fa82e6f0467d6269f7deaf65c16694b38df9e
Annie Taylor CHEN <annietaylorchen@gmail.com>
2019-08-26 21:17:12 +0200

    finish story page.

--- client/src/components/Story.js
+++ client/src/components/Story.js
@@ -3,7 +3,71 @@ import React from 'react'
 export default function Story() {
     return (
         <>
-            STORY
+            <section className="story-intro">
+                <img src="/imgs/mars.jpg" alt="thirty seconds to mars intro" />
+                <h2 className="story-intro-text">The Story</h2>
+            </section>
+
+            <section className="mars-bg mars-bg2018">
+                <span className="story-year story-year--right">2018</span>
+                <img src="/imgs/mars-2018.jpg" alt="mars band in 2018" />
+                <div className="mars-text mars-text--right">
+                    <h2>2015–present: America </h2>
+                    <p>
+                    On November 3, 2015, it was announced that Thirty Seconds to Mars are working on their fifth studio album. In August 2016, the band revealed to have signed to Interscope Records. The group later unveiled that they would embark on a North American tour with Muse and PVRIS, which took take place from May to September 2017. In August 2017, "Walk on Water" was announced as the lead single from the band's fifth album. Thirty Seconds to Mars then performed the song at the 2017 MTV Video Music Awards featuring special guest Travis Scott. During the ceremony, Jared Leto received media attention for his tribute to musicians Chester Bennington and Chris Cornell, who both died earlier that year.
+                    </p>
+                    <p>
+                    On January 25, 2018, Thirty Seconds to Mars released "Dangerous Night" as the second single from their upcoming fifth studio album. In February 2018, the band officially announced the Monolith Tour with Walk the Moon, Misterwives, K. Flay, Joywave, and Welshly Arms. Thirty Seconds to Mars later confirmed America as title of their fifth album, which was released on April 6, 2018.
+                    </p>
+                </div>
+            </section>
+
+            <section className="mars-bg mars-bg2013">
+                <span className="story-year story-year--left">2013</span>
+                <img className="mars-img-right" src="/imgs/mars-2013.jpg" alt="mars band in 2013" />
+                <div className="mars-text mars-text--left">
+                    <h2>2012–15: Love, Lust, Faith and Dreams</h2>
+                    <p>
+                    Thirty Seconds to Mars spent most of the year recording their fourth album, entitled Love, Lust, Faith and Dreams. The album was produced by Jared Leto with previous collaborator Steve Lillywhite. Leto said that the band took a new direction with Love, Lust, Faith and Dreams. He explained that the album "is more than an evolution, it's a brand new beginning. Creatively, we've gone to an entirely new place, which is exciting, unexpected, and incredibly inspiring." 
+                    </p>
+                    <p>
+                    In partnership with NASA, Thirty Seconds to Mars launched the first copy of "Up in the Air" aboard the Dragon spacecraft on SpaceX CRS-2, sending the first ever commercial copy of music into space. On March 18, 2013, the single premiered from the International Space Station. "Up in the Air" made its radio debut on March 18 and became commercially available for downloading the following day. Thirty Seconds to Mars released Love, Lust, Faith and Dreams on May 21, 2013, through Universal in the United States. 
+                    </p>
+                </div>
+            </section>
+
+            <section className="mars-bg mars-bg2009">
+                <span className="story-year story-year--right">2009</span>
+                <img src="/imgs/mars-2009.jpg" alt="mars band in 2009" />
+                <div className="mars-text mars-text--right">
+                    <h2>2008–11: This Is War</h2>
+                     <p>
+                    Thirty Seconds to Mars began recording their third studio album This Is War in August 2008. In a bid to involve their fans for This Is War, Thirty Seconds to Mars held an event, called The Summit, at the Avalon Club in Los Angeles, where they invited fans to provide backing vocals and percussion. After the success of the initial Summit, the group repeated the event in eight countries and extended it digitally. The band also invited fans to submit close-up shots of their faces in order to make 2,000 different individual covers for the album. Leto described This Is War as a record about survival: "It was a two-year creative battle that was ferocious and tough but creatively rewarding, and all of those adverse elements, in hindsight, made us stronger and made the record stronger." Although the release date was changed many times, This Is War was eventually released in December 2009. Its first two singles, "Kings and Queens" and "This Is War", reached the number-one spot on the US Alternative Songs chart. After a promotional tour in winter 2009, Thirty Seconds to Mars embarked on their Into the Wild Tour in February 2010.
+                    </p>
+                </div>
+            </section>
+
+            <section className="mars-bg mars-bg2005">
+                <span className="story-year story-year--left">2005</span>
+                <img className="mars-img-right" src="/imgs/mars-2005.jpg" alt="mars band in 2005" />
+                <div className="mars-text mars-text--left">
+                    <h2>2004–08: A Beautiful Lie</h2>
+                    <p>
+                    Thirty Seconds to Mars returned to the studio in March 2004 to begin working on their second album A Beautiful Lie, with Josh Abraham producing. During the recording process, the band traveled to four different continents to accommodate Jared Leto's acting career. A Beautiful Lie was notably different from the band's debut album, from both musical and lyrical aspect. "On the first record I created a world, then hid behind it," Leto said. "With A Beautiful Lie, it was time to take a more personal and less cerebral approach. Although this record is still full of conceptual elements and thematic ideas it is ultimately much more wrapped around the heart than the head. It's about brutal honesty, growth, change. It's an incredibly intimate look into a life that is in the crossroads. A raw emotional journey. A story of life, love, death, pain, joy, and passion. Of what it is to be human."
+                    </p>
+                </div>
+            </section>
+
+            <section className="mars-bg mars-bg2002">
+                <span className="story-year story-year--right">2002</span>
+                <img src="/imgs/mars-2002.jpg" alt="mars band in 2002" />
+                <div className="mars-text mars-text--right">
+                    <h2>2001–03: Debut album</h2>
+                    <p>
+                    The band released their first studio album, 30 Seconds to Mars, on August 27, 2002 in the United States through Immortal and Virgin. Jared Leto described the record as a concept album that focuses on human struggle and self-determination, in which otherworldly elements and conceptual ideas are used to illustrate a truthful personal situation. The album reached number 107 on the US Billboard 200 and number one on the US Top Heatseekers, selling 121,000 copies in the United States. It was preceded by the single "Capricorn (A Brand New Name)", which peaked at number 31 on the US Mainstream Rock chart. 
+                    </p>
+                </div>
+            </section>
         </>
     )
 }
