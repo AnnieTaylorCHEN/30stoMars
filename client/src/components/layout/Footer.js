448c30fe337bbbe22a05ef8291fe2ca77d2b19f4
Annie Taylor CHEN <annietaylorchen@gmail.com>
2019-09-04 13:49:27 +0200

    add full url name for atc.

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <div>
             <section className="footer__copyright">
-            <p>Handmade with &#10084; by <a href="https://annietaylorchen.com/" target="_blank" rel="noopener noreferrer">Annie Taylor Chen</a></p>
+            <p>Handmade with &#10084; by <a href="https://www.annietaylorchen.com/" target="_blank" rel="noopener noreferrer">Annie Taylor Chen</a></p>
             </section>
         </div>
     )
